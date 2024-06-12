106f1a825d4bc841a9d07d4a3a65575456b37271
Joaquin1128 <Joaquin.augusto@hotmail.com>
2024-06-12 12:14:09 -0300

    implementacion model y config app properties

---
+++ src/main/java/com/api/crud/models/BookModel.java
@@ -0,0 +1,58 @@
+package com.api.crud.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="book")
+public class BookModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int anioPublicacion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+        public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+}
